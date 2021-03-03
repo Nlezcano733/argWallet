6d480be325ccf45e618431ae373461ae75c2d975
Nahuel Lezcano <nlezcano733@gmail.com>
2021-03-03 02:40:47 -0300

    version 3.2.0 -- añadiendo interaccion con AJAX en Home

--- js/funcionesHome.js
+++ js/funcionesHome.js
@@ -22,6 +22,26 @@ function tomarJsonHome() {
     }
 }
 
+function getAjaxInicio (){
+    $.ajax({
+        url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=ars&order=market_cap_desc&per_page=8&page=1&sparkline=false",
+        type: "GET",
+        dataType: "json"
+    }).done((resultado)=>{
+        for(i=0; i<8; i++){
+            let objetosSeparados = resultado[i];
+            let listadoGet = JSON.stringify(objetosSeparados);
+            let objetos = JSON.parse(listadoGet);
+            carteraMuestraInicio.push(objetos);
+        }
+        armadoDeLista();
+        armadoDeCriptos();
+        cambiarCriptoMostrada();
+    }).fail(()=>{
+        console.log('fallo')
+    })
+}
+
 // ------------------------------------------------------------------ //
 // ------------------------------------------------------------------ //
 // ------------------------------------------------------------------ //
@@ -74,8 +94,8 @@ function armadoDeLista() {
 function armadoDeCriptos() {
     for (let i = 0; i < carteraMuestraInicio.length; i++) {
         let posicion = carteraMuestraInicio[i];
-        let nombre = posicion.nombre;
-        let foto = posicion.logo;
+        let nombre = posicion.nombre; // let nombre = posicion.id
+        let foto = posicion.logo; // let foto = posicion.image
         crearCriptosInicio(nombre, i)
         crearFotosInicio(foto, nombre, i);
     }
@@ -94,7 +114,7 @@ function cambiarCriptoMostrada(i) {
     $('#inicio__imagenCripto').fadeIn(2000, () => {
         let cripto = carteraMuestraInicio[i];
 
-        modificarFotoInicio('#inicio__imagenCripto', cripto.logo);
+        modificarFotoInicio('#inicio__imagenCripto', cripto.logo); // modificarFotoInicio('##inicio__imagenCripto', cripto.image)
         marcarCripto(i)
         i++;
         cambiarCriptoMostrada(i);
@@ -160,14 +180,50 @@ function avanzarBody(ubicacion) {
 }
 
 function scrollify() {
-    $(() => {
-        $.scrollify({
-            section: '.sectionScroll',
-            sectionName: '#footer'
-        });
+    $.scrollify({
+        section: '.sectionScroll'
     });
 }
 
+function deshabilitarScrollify (){
+    $.scrollify.disable();
+}
+function habilitarScrollify(){
+    $.scrollify.enable();
+}
+
+function scrollFinal (){
+    let tabla = $('#mercado__lista');
+
+    $(document).scroll(()=>{
+        let seccionActual = window.location.hash;
+
+        if(seccionActual == '#3'){
+            $(tabla).hover(()=>{
+                deshabilitarScrollify();
+                scrollTabla()
+                deshabilitarScroll();
+            }, ()=>{
+                habilitarScroll()
+                habilitarScrollify();
+            })
+        }
+    })
+}
+function scrollTabla (){
+    $.scrollify.instantMove('#mercado__lista')
+}
+function deshabilitarScroll(){
+    let top = $('#mercado');
+    top = top[0].offsetTop;
+    
+    window.onscroll = () =>{
+        window.scrollTo(0, top)
+    }
+}
+function habilitarScroll (){
+    window.onscroll = null;
+}
 
 // ---------------------------------------------- //
 // ---------------------------------------------- //
@@ -207,4 +263,207 @@ function ingresoConversor(id){
     $(id).click(()=>{
         $(location).attr('href', 'conversor.html');
     })
+}
+
+
+// ---------------------------------------------- //
+// ---------------------------------------------- //
+// ---------------------------------------------- //
+
+function getAjaxMercado(){
+    $.ajax({
+        url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=ars&order=market_cap_desc&per_page=25&page=1&sparkline=false",
+        type: "GET",
+        dataType: "json"
+    }).done((resultado)=>{
+        armadoDeTabla(resultado, 'ars');
+    })
+}
+
+function getAjaxModMercado(moneda){
+    $.ajax({
+        url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=" + moneda + "&order=market_cap_desc&per_page=25&page=1&sparkline=false",
+        type: "GET",
+        dataType: "json"
+    }).done((resultado)=>{
+        modificarTabla(resultado, moneda);
+    })
+}
+
+function cambioMuestraDivisa(){
+    let selector = $('#lista__cabecera--divisas');
+    
+    $(selector).change(()=>{
+        let monedaElegida = $('#lista__cabecera--divisas option:selected').text();
+        monedaElegida = monedaElegida.toLowerCase();
+        getAjaxModMercado(monedaElegida)
+    })
+}
+
+function armadoDeTabla(resultado, moneda){
+    i=0
+    resultado.forEach((cripto)=>{
+        let criptoNombre = nombreDeCripto(cripto, cripto);
+        let criptoImagen = cripto.image;
+        let criptoPar = parDeConversion(cripto, moneda);
+
+        let criptoPrecio = cripto.current_price;
+        let cambio = cripto.price_change_percentage_24h;
+        let criptoCambio = porcentajeDeCambio(cambio)
+
+        let criptoMax = cripto.high_24h;
+        let criptoMin = cripto.low_24h;
+        let criptoVol = volumenOperado(cripto);
+
+        crearTr('tbody', 'lista__cripto');
+        armarTr(criptoImagen, criptoNombre, criptoPar, criptoPrecio, criptoCambio, criptoMax, criptoMin, criptoVol, i);
+        i++;
+    })
+}
+
+function modificarTabla(resultado, moneda){
+    let i=0;
+    resultado.forEach((cripto)=>{
+        let criptoPar = parDeConversion(cripto, moneda);
+
+        let criptoPrecio = cripto.current_price;
+        let cambio = cripto.price_change_percentage_24h;
+        let criptoCambio = porcentajeDeCambio(cambio)
+
+        let criptoMax = cripto.high_24h;
+        let criptoMin = cripto.low_24h;
+        let criptoVol = volumenOperado(cripto);
+        
+        modificarElementosDeTabla(criptoPar, criptoPrecio, criptoCambio, criptoMax, criptoMin, criptoVol, i);
+        i++;
+    })
+}
+
+function modificarElementosDeTabla(par, precio, cambio, max, min, vol, i){
+    let classPar = $('.lista__cripto--par')
+    let classPrecio = $('.lista__cripto--precio')
+    let classCambio = $('.lista__cripto--cambio')
+    let classMax = $('.lista__cripto--max')
+    let classMin = $('.lista__cripto--min')
+    let classVol = $('.lista__cripto--vol')
+
+
+    modificarElemento(classPar[i], par)
+    modificarElemento(classPrecio[i], precio)
+    modificarElemento(classCambio[i], cambio)
+    modificarElemento(classMax[i], max)
+    modificarElemento(classMin[i], min)
+    modificarElemento(classVol[i], vol)
+}
+
+function modificarElemento(elemento, contenido){
+    $(elemento).text(contenido)
+}
+
+function nombreDeCripto ({id}, {symbol}){
+        nombre = primeraLetraMayuscula(id);
+        let ticker = symbol.toUpperCase();
+        return `${nombre} (${ticker})`
+}
+
+function primeraLetraMayuscula (string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+function parDeConversion ({symbol}, moneda){
+    let par = `${symbol}/${moneda}`
+    return par.toUpperCase();
+}
+
+function porcentajeDeCambio (cambio){
+    criptoCambio = parseFloat(cambio).toFixed(2)
+    return `${criptoCambio} %`
+}
+
+function volumenOperado ({total_volume}){
+    let criptoVol = total_volume;
+    let millon = 1000000;
+    let billon = millon * 1000;
+
+    if(criptoVol >= billon){
+        criptoVol = parseFloat(criptoVol / billon).toFixed(2);
+        return `${criptoVol} B`
+    }
+    if(criptoVol >= millon){
+        criptoVol = parseFloat(criptoVol / millon).toFixed(2);
+        return `${criptoVol} M`
+    }
+}
+
+function crearTr(nodoPadre, nombreClase){
+    let nuevoNodo;
+    padre = $(nodoPadre);
+
+    nuevoNodo = document.createElement('tr');
+    $(nuevoNodo).attr('class', nombreClase);
+    $(nodoPadre).append(nuevoNodo);
+}
+
+function armarTr (logo, nombre, par, precio, cambio, max, min, vol, i){
+
+    crearElemento('.lista__cripto', 'td', 'class', 'lista__cripto--td', '', i);
+    crearDivClassPadre('.lista__cripto--td', 'class', 'lista__cripto--nombre', i)
+
+    crearImagen('.lista__cripto--nombre', logo, 'class', 'imagenCripto', i)
+    crearElemento('.lista__cripto--nombre', 'td', 'class', 'nombreCriptoLista', nombre, i);
+
+    crearElemento('.lista__cripto', 'td', 'class', 'lista__cripto--par', par, i);
+    crearElemento('.lista__cripto', 'td', 'class', 'lista__cripto--precio', precio, i);
+    crearElemento('.lista__cripto', 'td', 'class', 'lista__cripto--cambio', cambio, i);
+    crearElemento('.lista__cripto', 'td', 'class', 'lista__cripto--max', max, i);
+    crearElemento('.lista__cripto', 'td', 'class', 'lista__cripto--min', min, i);
+    crearElemento('.lista__cripto', 'td', 'class', 'lista__cripto--vol', vol, i);
+
+    crearElemento('.lista__cripto', 'td', 'class', 'lista__cripto--btnEspacio', '', i)
+    crearBtn('.lista__cripto--btnEspacio', 'class', 'lista__cripto--btn', i)
+}
+
+function crearDivClassPadre (classPadre, attr, nombreAttr, i){
+    let nuevoNodo, nodoPadre;
+    nodoPadre = $(classPadre);
+    nuevoNodo = document.createElement('div');
+    $(nuevoNodo).attr(attr, nombreAttr);
+    $(nodoPadre[i]).append(nuevoNodo);
+}
+
+function crearElemento (padre, tag, attr, nombreAttr, contenido, i){
+    let nuevoNodo, nodoPadre;
+    nodoPadre = $(padre);
+    nuevoNodo = document.createElement(tag);
+    contenidoTexto = document.createTextNode(contenido);
+
+    $(nuevoNodo).append(contenidoTexto);
+    $(nuevoNodo).attr(attr, nombreAttr);
+    $(nodoPadre[i]).append(nuevoNodo);
+}
+
+
+function crearBtn (padre, attr, nombreAttr, i){
+    let nuevoNodo, nodoPadre;
+    nodoPadre = $(padre);
+    nuevoNodo = document.createElement('a');
+    contenidoTexto = document.createTextNode('Ver');
+    $(nuevoNodo).append(contenidoTexto);
+
+    $(nuevoNodo).attr('href', '#');
+    $(nuevoNodo).attr('role', 'button');
+    $(nuevoNodo).attr(attr, nombreAttr);
+
+    $(nodoPadre[i]).append(nuevoNodo)
+}
+
+function crearImagen (padre, direccion, attr, nombreAttr, i){
+    let nuevoNodo, nodoPadre;
+    nodoPadre = $(padre);
+    nuevoNodo = document.createElement('img');
+
+    $(nuevoNodo).attr('src', direccion);
+    $(nuevoNodo).attr(attr, nombreAttr);
+
+    $(nodoPadre[i]).append(nuevoNodo)
 }
--- js/main.js
+++ js/main.js
@@ -1,20 +1,36 @@
 let carteraCriptos = [];
 let carteraMuestraInicio = [];
 
+
 $(()=>{
+    //MODIFICABLE AL APLICAR AJAX -- activar si se arma lista con json 
     tomarJson()
     tomarJsonHome();
+    // getAjaxInicio();
 
     avanzarNavbar();
     scrollify();
+    scrollFinal();
     slider();
+
+// -------------------------------------- //
+// HABILITAR EN CASO DE ARMAR LA LISTA CON OBJETO JSON
+// ver setInterval
 
     armadoDeLista();
     armadoDeCriptos();
     cambiarCriptoMostrada();
+// -------------------------------------- //
 
     mostrarPanelIngreso();
-    
     //Funcion parcial hasta que se defina algoritmo logIn
     ingresoConversor('.panelIngreso__btn');
+
+// -------------------------------------- //
+
+    getAjaxMercado();
+    cambioMuestraDivisa();
 })
+
+
+
